146d789aa3b1a57e5481a1632c50a0bab326a659
Arambulo Avila <A446486@santandertecnologia.com.ar>
2022-05-16 18:18:19 -0300

    desabilitados los botones para cargar formularios hasta completar los campos

---
+++ public/js/loginScript.js
@@ -0,0 +1,17 @@
+
+let ableLogin = () =>{
+    let name = document.querySelector('.email').value
+    let password = document.querySelector('.password').value;
+    let validate = 0;
+
+    if(name == '' || password ==''){
+        validate ++;
+    }
+
+    validate == 0 ? document.querySelector('.login_button').disabled = false :
+    document.querySelector('.login_button').disabled = true;
+
+}
+
+document.querySelector('.email').addEventListener('keyup', ableLogin);
+document.querySelector('.password').addEventListener('keyup', ableLogin);
---
+++ public/js/registerScript.js
@@ -0,0 +1,22 @@
+let ableRegister = () =>{
+    let regName = document.querySelector('.regName').value;
+    let regLName = document.querySelector('.regLName').value;
+    let regEmail = document.querySelector('.regEmail').value;
+    let regPass1 = document.querySelector('.regPass1').value;
+    let regPass2 = document.querySelector('.regPass2').value;
+    let validate = 0;
+
+    if(regName== '' || regLName =='' || regEmail =='' || regPass1 =='' || regPass2 ==''){
+        validate ++;
+    }
+
+    validate == 0 ? document.querySelector('.login_button').disabled = false :
+    document.querySelector('.login_button').disabled = true;
+
+}
+document.querySelector('.regName').addEventListener('keyup', ableRegister);
+document.querySelector('.regLName').addEventListener('keyup', ableRegister);
+document.querySelector('.regEmail').addEventListener('keyup', ableRegister);
+document.querySelector('.regPass1').addEventListener('keyup', ableRegister);
+document.querySelector('.regPass2').addEventListener('keyup', ableRegister);
+
