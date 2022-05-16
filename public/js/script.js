5dd9861538a36bd3093fef2b9f5aad2ea90aec67
Giuliano <gdepalma@santandertecnologia.com.ar>
2022-05-16 09:31:21 -0300

    comenzando click menu

---
+++ public/js/script.js
@@ -0,0 +1,22 @@
+window.addEventListener("load",()=>{
+
+    //profile
+    let profile = document.querySelector(".profile");
+    let miniprofile = document.querySelector(".miniprofile");
+    profile.addEventListener("click", (e)=>{
+        miniprofile.classList.toggle("show");
+    })
+    
+    /*
+    document.addEventListener("click", (e)=>{
+        if(miniprofile.classList.contains("show")&&
+        (window.screen.availWidth<414 && e.clientX<130)||(window.screen.availWidth>=414 && (e.screenX<635 || e.screenY>430 || e.screenY<215))){
+            miniprofile.classList.toggle("show");
+        }
+    });
+    */
+    
+    
+
+
+})
