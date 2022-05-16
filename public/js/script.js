f914b4b7a11fb2657d2335e81a47785ed92d21c5
Giuliano <gdepalma@santandertecnologia.com.ar>
2022-05-16 15:24:52 -0300

    responsive tablet

--- public/js/script.js
+++ public/js/script.js
@@ -7,14 +7,15 @@ window.addEventListener("load",()=>{
         miniprofile.classList.toggle("show");
     })
     
-    /*
-    document.addEventListener("click", (e)=>{
-        if(miniprofile.classList.contains("show")&&
-        (window.screen.availWidth<414 && e.clientX<130)||(window.screen.availWidth>=414 && (e.screenX<635 || e.screenY>430 || e.screenY<215))){
-            miniprofile.classList.toggle("show");
-        }
-    });
-    */
+    
+    // document.addEventListener("click", (e)=>{
+    //     // if(miniprofile.classList.contains("show")&&
+    //     // (window.screen.availWidth<414 && e.clientX<130)||(window.screen.availWidth>=414 && (e.screenX<635 || e.screenY>430 || e.screenY<215))){
+    //     //     miniprofile.classList.toggle("show");
+    //     // }
+    // });
+    
+   
     
     
 
