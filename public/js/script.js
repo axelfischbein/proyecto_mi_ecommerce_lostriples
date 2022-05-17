29106f152262ec18e75b7992562f99e0180f294f
Giuliano <gdepalma@santandertecnologia.com.ar>
2022-05-16 22:54:36 -0300

    iniciando js

--- public/js/script.js
+++ public/js/script.js
@@ -1,32 +1,12 @@
 window.addEventListener("load",()=>{
 
     //profile
-<<<<<<< HEAD
     document.querySelector(".profile").addEventListener("click", (e)=>{
         document.querySelector(".miniprofile").classList.toggle("show");
     })
     document.addEventListener("click", (e)=>{
         if (!e.target.classList.contains("prof")) document.querySelector(".miniprofile").classList.remove("show");
     });
-=======
-    let profile = document.querySelector(".profile");
-    let miniprofile = document.querySelector(".miniprofile");
-    
-    
-    // document.addEventListener("click", (e)=>{
-    //     if(miniprofile.classList.contains("show")&&
-    //     (window.screen.availWidth<414 && e.clientX<130)||(window.screen.availWidth>=414 && (e.screenX<635 || e.screenY>430 || e.screenY<215))){
-    //         miniprofile.classList.remove("show");
-    //     }
-    // });
-    
-    profile.addEventListener("click", (e)=>{
-        miniprofile.classList.toggle("show");
-    })
-   
-    
-    
->>>>>>> 146d789aa3b1a57e5481a1632c50a0bab326a659
 
 
 })
