604620b1627bc6887eb8e8a8261b38d4bea72423
Giuliano <gdepalma@santandertecnologia.com.ar>
2022-05-17 09:58:15 -0300

    logica carrito

--- public/js/cartScript.js
+++ public/js/cartScript.js
@@ -1,3 +1,21 @@
 window.addEventListener('load', ()=>{
-    console.log("ok");
+
+    document.querySelectorAll(".cart_item").forEach(item => {
+
+        item.querySelector(".agregarprod").addEventListener("click", (e)=>{
+            let id = e.target.parentNode.parentNode.parentNode.firstElementChild.firstElementChild.getAttribute("value");
+            fetch(`http://localhost:3030/api/agregarProducto/${id}`, {
+                method: "POST",
+            })
+            window.location.reload();
+        });
+        item.querySelector(".quitarprod").addEventListener("click", (e)=>{
+            let id = e.target.parentNode.parentNode.parentNode.firstElementChild.firstElementChild.getAttribute("value");
+            fetch(`http://localhost:3030/api/quitarProducto/${id}`, {
+                method: "DELETE",
+            })
+            window.location.reload();
+        });
+
+    });
 });
